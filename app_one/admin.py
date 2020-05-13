7e034a300d4697a860f35bd1f6b508a9d1344e0a
Donovan1905 <44325654+Donovan1905@users.noreply.github.com>
2020-05-13 18:41:17 +0200

    foreign key to company

--- app_one/admin.py
+++ app_one/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser, Company
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'company')
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
--- app_one/models.py
+++ app_one/models.py
@@ -1,3 +1,36 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class Company(models.Model):
+    name = models.CharField(_("name"), max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    company = models.ForeignKey(
+        'Company',
+        on_delete=models.CASCADE,
+        null=True,
+        blank = True
+    )
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
